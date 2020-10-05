726f19e33fa3b4756111888e79ae63fd1cf7b10b
gentaliana <gentaliana@gmail.com>
2020-10-05 19:52:37 +0400

    feat: add solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const num = String(n);
+    if (n < 0) {
+        const positiveNum = Math.abs(num);
+        const intNumToString = String(positiveNum);
+        const result = intNumToString.split("").reverse().join("");
+        return result;
+    }
+    return num.split("").reverse().join("");
+};
